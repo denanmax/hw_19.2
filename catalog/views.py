414a8e0f65409fdd5db09680b920232aa8dace48
denanmax <deshis93@gmail.com>
2023-07-28 00:13:44 +0200

    Initial commit

---
+++ catalog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone_number = request.POST.get('phone_number')
+        message = request.POST.get('message')
+        print(f"{name}, {phone_number}, {message}")
+    return render(request, 'catalog/contacts.html')
