cc6b585d76295ffa13e9323b0ca8f049a342bc61
denanmax <deshis93@gmail.com>
2023-08-02 20:48:50 +0200

    * added. last5 products on main page and contact form adding to db

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 
+from catalog.models import Product, Contact
+
+
 def home(request):
-    return render(request, 'catalog/home.html')
+    latest_products = Product.objects.order_by('-id')[:5]
+    for product in latest_products:
+        print(product.name)  # Пример вывода имени товара в консоль
+    return render(request, 'catalog/home.html', {'latest_products': latest_products})
+
 
 def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        phone_number = request.POST.get('phone_number')
+        phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f"{name}, {phone_number}, {message}")
-    return render(request, 'catalog/contacts.html')
+        contact = Contact(name=name, phone=phone, message=message)
+        contact.save()
+        print(f"{name}, {phone}, {message}")
+
+    contacts = Contact.objects.all()
+    return render(request, 'catalog/contacts.html', {'contacts': contacts})
