1390f16f07f303c112c2443b833fadb5d31277af
denanmax <deshis93@gmail.com>
2023-08-23 13:38:04 +0300

    model Version

--- catalog/models.py
+++ catalog/models.py
@@ -12,7 +12,7 @@ NULLABLE = {'blank': True, 'null': True}
 
 class Category(models.Model):
     name = models.CharField(max_length=50, blank=False, verbose_name='категория')
-    description = models.TextField(verbose_name='описание')
+    description = models.TextField(verbose_name='описание', blank=False)
 
     def __str__(self):
         return f'{self.name}, {self.description}'
@@ -26,10 +26,10 @@ class Product(models.Model):
     name = models.CharField(max_length=50, blank=False, verbose_name='наименование')
     description = models.TextField(verbose_name='описание')
     preview_image = models.ImageField(upload_to='product_image/', **NULLABLE, verbose_name='изображение продукта')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1,verbose_name='категория')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     creation_date = models.DateTimeField(auto_now_add=True)
-    last_change_date = models.DateTimeField(auto_now=True)
+    last_change_date = models.DateTimeField(auto_now=True, **NULLABLE)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
@@ -39,6 +39,17 @@ class Product(models.Model):
         ordering = ('name',)
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+class Version(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
+    version_number = models.IntegerField(verbose_name='номер версии')
+    version_name = models.CharField(max_length=50, verbose_name='название версии')
+    is_current = models.BooleanField(default=True, verbose_name='признак текущей версии', unique=True)
+
+    class Meta:
+        verbose_name = 'Версия'
+        verbose_name_plural = 'Версии'
+
 
 
 class Contact(models.Model):
