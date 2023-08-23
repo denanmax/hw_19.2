ccc5791c4290229f30de27d357b351ce70962ddc
denanmax <deshis93@gmail.com>
2023-08-23 13:38:26 +0300

    catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 from .models import toggle_activity
-from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, \
+    CategoryCreateView, CategoryListView, CategoryUpdateView, CategoryDeleteView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
@@ -11,4 +12,8 @@ urlpatterns = [
     path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
     path('activity/<int:pk>/', toggle_activity, name='toggle_activity'),
+    path('create_category/', CategoryCreateView.as_view(), name='create_category'),
+    path('category', CategoryListView.as_view(), name='category_list'),
+    path('category_edit/<int:pk>/', CategoryUpdateView.as_view(), name='edit_category'),
+    path('category_delete/<int:pk>/', CategoryDeleteView.as_view(), name='delete_category'),
 ]
