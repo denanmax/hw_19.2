07820883550ad7de1a6182752c1dbf32041f9efd
denanmax <deshis93@gmail.com>
2023-08-28 12:13:59 +0300

    catalog view_fixed

--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ from django.views.generic import CreateView, UpdateView, DetailView
 
 from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
-from utils.utils import confirm_user_email, generate_secret_key
+from utils.utils import confirm_user_email, create_secret_key
 
 
 # Create your views here.
@@ -34,5 +34,45 @@ class UserDetailView(DetailView):
         return self.request.user
 
 
+def confirm_email(request):
+    """подтверждаем почту"""
+    confirm_user_email(request, request.user)
+    return redirect(reverse('users:profile'))
 
 
+def activate_email(request, key):
+    """активируем почту"""
+    print(request.user.email_confirm_key, key, sep='\n')
+    if request.user.email_confirm_key == key:
+        request.user.email_is_confirmed = True
+        request.user.email_confirm_key = None
+        request.user.save()
+    else:
+        print('Ключ не актуален')
+    return redirect('/')
+
+
+def password_reset(request):
+    """генерируем пароль для неавторизованного пользователя"""
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        try:
+            user = User.objects.get(email=email)
+            new_password = create_secret_key(12)
+            user.set_password(new_password)
+            user.save()
+            login(request, user)
+
+            send_mail(
+                subject='Вы сменили пароль',
+                message=f'Новый пароль {new_password}',
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[request.user.email]
+            )
+
+            return redirect(reverse("users:login"))# все ок -> логин
+        # не ок -> Error message + остаемся тут же
+        except User.DoesNotExist:
+            return render(request, 'users/password_reset_form.html',
+                          {'error_message': 'Такого пользователя не существует'})
+    return render(request, 'users/password_reset_form.html')
