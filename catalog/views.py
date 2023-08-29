690e0f564dbf71c6a820ec7cccc16889f71f0855
denanmax <deshis93@gmail.com>
2023-08-29 19:51:29 +0300

    23.1 moderator

--- catalog/views.py
+++ catalog/views.py
@@ -55,8 +55,31 @@ class ProductCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView)
         self.object.save()
         return super().form_valid(form)
 
+class ProductModeratorUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    permission_required = 'catalog.change_product'
+    success_url = reverse_lazy('home')
 
-class ProductUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = VersionFormset(instance=self.object)
+
+        return context_data
+
+    def form_valid(self, form):
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     permission_required = 'catalog.change_product'
